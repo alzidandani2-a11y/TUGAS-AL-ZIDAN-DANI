cde314e4f9fc5e96f2b5ea1f5fd3f3cce4adb229
alzidandani2-a11y <alzidandani2@gmail.com>
2025-10-01 10:18:45 +0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+nama_depan = "al zidan"
+nama_belakang = "dani"
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '18'
+alamat = 'toboali'
+kata_mutiara = "jangan pernah berhenti mengambil semua kesempatan karena lebih baik gagal setelah mencoba dari pada tidak sama sekali"
+print(nama_lengkap, '(' + usia + ')',',', 'dari', alamat, ',kata mutiara:', kata_mutiara)
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:',type(usia))
+print('Tipe dari alamat:',type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
